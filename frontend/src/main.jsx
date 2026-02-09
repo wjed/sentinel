7ff02e4dda2dcc59c6164c11d76193c84bc36274
Nicky5x <franknv@dukes.jmu.edu>
2026-02-09 10:22:21 -0500

    revert test
    undo pp

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App /> pp
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 )
