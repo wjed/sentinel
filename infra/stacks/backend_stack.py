ddeb4c1eaf5f0481ad38cce2c05b88aa144e1c02
x4ping <x4ping@gmail.com>
2026-02-23 10:38:55 -0500

    fixed naming issue

--- infra/stacks/backend_stack.py
+++ infra/stacks/backend_stack.py
@@ -15,7 +15,7 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class DataStack(Stack):
+class BackendStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, vpc: ec2.Vpc, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
